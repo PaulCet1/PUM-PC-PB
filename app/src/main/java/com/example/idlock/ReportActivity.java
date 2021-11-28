31f0121c43b9dba55c042a797545c8747e07752f
Piotr Bożek <piotrbozek2000@gmail.com>
2021-11-28 16:19:08 +0100

    laouts funkcji liczenia kroków

---
+++ app/src/main/java/com/example/idlock/ReportActivity.java
@@ -0,0 +1,14 @@
+package com.example.idlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report);
+    }
+}
---
+++ app/src/main/java/com/example/idlock/TrackStepsActivity.java
@@ -0,0 +1,14 @@
+package com.example.idlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrackStepsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.track_steps);
+    }
+}
