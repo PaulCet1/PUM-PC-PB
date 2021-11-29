d5dabaf33fb7d92a75644c18467b600d894bf6bb
Paweł <studentpawel2000@gmail.com>
2021-11-29 22:14:26 +0100

    Lokalizacja google map

--- app/src/main/java/com/example/idlock/mapactivity.java
+++ app/src/main/java/com/example/idlock/mapactivity.java
@@ -1,18 +1,26 @@
 package com.example.idlock;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
-public class mapactivity extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
+public class mapactivity extends AppCompatActivity {
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +28,62 @@ public class mapactivity extends FragmentActivity implements OnMapReadyCallback 
         setContentView(R.layout.activity_mapactivity);
 
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(mapactivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            getCurrentLocation();
+
+
+        } else {
+            ActivityCompat.requestPermissions(mapactivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
 
     }
 
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(location -> {
+            if (location != null) {
+                supportMapFragment.getMapAsync(googleMap -> {
+                    LatLng latLng = new LatLng(location.getLatitude()
+                            , location.getLongitude());
+
+                    MarkerOptions options = new MarkerOptions().position(latLng)
+                            .title("Tutaj jestem!");
+
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+                    googleMap.addMarker(options);
+                });
+
+            }
+        });
+    }
+
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        map = googleMap;
-        LatLng NS = new LatLng(49.6217700, 20.6970500);
-        map.addMarker(new MarkerOptions().position(NS).title("Nowy Sącz"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(NS));
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+        }
     }
 }
